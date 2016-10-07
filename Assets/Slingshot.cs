c65dfe7b904d1a7670aab093cfb45b4adddb9845
Kevin W <wnivek@gmail.com>
2016-10-06 20:34:52 -0400

    Clicking slingshot spawns stational projectile

--- Assets/Slingshot.cs
+++ Assets/Slingshot.cs
@@ -2,13 +2,43 @@
 using System.Collections;
 
 public class Slingshot : MonoBehaviour {
+
+    // fields set in the Unity Inspector page
+    public GameObject prefabProjectile;
+    public bool ________________________; // silly way to give ourselves a separator in the inspector
+    // fields set dynamically
+    public GameObject launchPoint;
+    public Vector3 launchPos;
+    public GameObject projectile;
+    public bool aimingMode;
+
+    void Awake() {
+        Transform launchPointTrans = transform.Find("LaunchPoint");
+        launchPoint = launchPointTrans.gameObject;
+        launchPoint.SetActive(false);
+        launchPos = launchPointTrans.position;
+    }
     
     void OnMouseEnter() {
-        print("Slingshot:OnMouseEnter()");
+        //print("Slingshot:OnMouseEnter()");
+        launchPoint.SetActive(true);
     }
 
     void OnMouseExit() {
-        print("Slingshot:OnMouseExit()");
+        //print("Slingshot:OnMouseExit()");
+        launchPoint.SetActive(false);
+    }
+
+    void OnMouseDown() {
+        // The player has pressed the mouse button while over Slingshot
+        aimingMode = true;
+        // Instantiate a Projectile
+        projectile = Instantiate(prefabProjectile) as GameObject;
+        // Start it at the launchPoint
+        projectile.transform.position = launchPos;
+        // Set it to isKinematic for now
+        //projectile.rigidbody.isKinematic = true;
+        projectile.GetComponent<Rigidbody>().isKinematic = true;
     }
 
 }
