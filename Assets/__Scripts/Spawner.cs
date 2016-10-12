ba965c68094847783aa479bb97a107dd22e17889
Kevin Waters <wnivek@gmail.com>
2016-10-11 23:33:04 -0400

    Exported first castle as prefab; created 'spawner' objects so walls within castle prefab would continue to follow the original wall prefabs

---
+++ Assets/__Scripts/Spawner.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour {
+
+    public GameObject prefabSpawnee;
+
+    // Use this for initialization
+    void Start () {
+        GameObject tGO = Instantiate(prefabSpawnee) as GameObject;
+        tGO.transform.SetParent(transform.parent);
+        tGO.transform.localPosition = transform.localPosition;
+        tGO.transform.localRotation = tGO.transform.localRotation * transform.localRotation;
+        Vector3 scale = tGO.transform.localScale;
+        scale.Scale(transform.localScale);
+        tGO.transform.localScale = scale;
+        Destroy(this.gameObject);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
